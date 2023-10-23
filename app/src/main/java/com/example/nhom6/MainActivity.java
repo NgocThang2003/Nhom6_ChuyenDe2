760fb1b92bea98352bf2481275b7027117b296d0
thuy123 <21211tt1986@mail.tdc.edu.vn>
2023-10-23 18:14:32 +0700

    làm tiếp form giỏ hàng

--- app/src/main/java/com/example/nhom6/MainActivity.java
+++ app/src/main/java/com/example/nhom6/MainActivity.java
@@ -9,10 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.manhinhdangnhap);
-        //asdđfff
-        //uieroorofk
-        //aaaa
-        //aaaaaa
+        setContentView(R.layout.giohang);
+
     }
 }
