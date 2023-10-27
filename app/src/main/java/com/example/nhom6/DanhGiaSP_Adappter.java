ef94760ee447522723a603e1afd0d2863e496aec
thuy123 <21211tt1986@mail.tdc.edu.vn>
2023-10-27 20:13:26 +0700

    làm tiếp form đánh giá SP

--- app/src/main/java/com/example/nhom6/DanhGiaSP_Adappter.java
+++ app/src/main/java/com/example/nhom6/DanhGiaSP_Adappter.java
@@ -1,4 +1,34 @@
 package com.example.nhom6;
 
-public class DanhGiaSP_Adappter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DanhGiaSP_Adappter extends RecyclerView.Adapter<DanhgiaSP_Holder>{
+    Context context;
+    public DanhGiaSP_Adappter(Context context, List<DanhGiaSP> data) {
+        this.context = context;
+        this.data = data;
+    }
+    List<DanhGiaSP> data;
+    @NonNull
+    @Override
+    public DanhgiaSP_Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DanhgiaSP_Holder(LayoutInflater.from(context).inflate(R.layout.item_tatca,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DanhgiaSP_Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
