74e1a34fca90144faccb3f012fa82f25288046c1
Thang <21211tt2630@mail.tdc.edu.vn>
2023-10-30 21:01:44 +0700

    Menu+

---
+++ app/src/main/java/com/example/nhom6/MainActivity_LienHeNhanVien.java
@@ -0,0 +1,14 @@
+package com.example.nhom6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_LienHeNhanVien extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lienhenhanvien);
+    }
+}
