20a77c02266efd2d5d1435f2261bba9c3f2b567f
Thang <21211tt2630@mail.tdc.edu.vn>
2023-11-10 23:04:25 +0700

    Don Hang Shipper, Don Hang Kho

---
+++ app/src/main/java/com/example/nhom6/Shipper_DonHangCuaBan_Adapter.java
@@ -0,0 +1,75 @@
+package com.example.nhom6;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Shipper_DonHangCuaBan_Adapter extends RecyclerView.Adapter<Shipper_DonHangCuaBan_Holder> {
+    Context context;
+    List<DonHang> data;
+    public Shipper_DonHangCuaBan_Adapter(Context context, List<DonHang> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public Shipper_DonHangCuaBan_Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new Shipper_DonHangCuaBan_Holder(LayoutInflater.from(context).inflate(R.layout.item_shipper_donhangcuaban,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Shipper_DonHangCuaBan_Holder holder, int position) {
+        DonHang donHang = data.get(position);
+
+        holder.tvTenKH.setText(donHang.tenKhachHang);
+        if (donHang.hinh.trim().equals("")){
+            holder.ivHinh.setImageResource(R.drawable.giongngo);
+        }
+        else {
+            try {
+                byte[] bytes = chuyenStringSangByte(donHang.hinh);
+                Bitmap bitmap = chuyenByteSangBitMap(bytes);
+                holder.ivHinh.setImageBitmap(bitmap);
+            }
+            catch (Exception e){
+                holder.ivHinh.setImageResource(R.drawable.giongngo);
+            }
+        }
+        holder.tvDiaChi.setText(donHang.diaChi);
+        holder.tvTenSP.setText(donHang.tenSanPham);
+        holder.tvMoTa.setText(donHang.moTa);
+        holder.tvSDT.setText(donHang.sDT);
+        holder.tvGia.setText(donHang.gia);
+        holder.tvSoLuong.setText(donHang.soLuong);
+        holder.tvNgay.setText(donHang.ngay);
+        holder.tvTrangThai.setText(donHang.trangThai);
+        int gia = Integer.parseInt(donHang.gia.trim());
+        int soLuong = Integer.parseInt(donHang.soLuong.trim());
+
+        holder.tvThanhTien.setText("" + gia * soLuong);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    private byte[] chuyenStringSangByte(String str) {
+        byte[] byteArray = android.util.Base64.decode(str, android.util.Base64.NO_PADDING | android.util.Base64.NO_WRAP | android.util.Base64.URL_SAFE);
+        return byteArray;
+    }
+
+    //Chuyen byte[] sang bitMap
+    private Bitmap chuyenByteSangBitMap(byte[] byteArray) {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        return bitmap;
+    }
+}
