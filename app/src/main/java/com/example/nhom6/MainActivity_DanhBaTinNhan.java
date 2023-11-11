44ab53c36a06b2bf6d8790db6d40b4c548801544
xuantuan <xuantuanbmt753753@gmail.com>
2023-11-11 16:20:35 +0700

    quan tri chung cac doanh muc vs FireBase

---
+++ app/src/main/java/com/example/nhom6/MainActivity_DanhBaTinNhan.java
@@ -0,0 +1,14 @@
+package com.example.nhom6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_DanhBaTinNhan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_danh_ba_tin_nhan);
+    }
+}
